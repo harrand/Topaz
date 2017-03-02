f414e8abcbf414e0c70bfcb88336b90123dde441
Harry <harrysjh98@gmail.com>
2017-03-02 17:08:44 +0000

    Enhanced lighting engine by fixing up shaders + adding specular components

--- res/runtime/shaders/vanilla.fragment.glsl
+++ res/runtime/shaders/vanilla.fragment.glsl
@@ -5,24 +5,39 @@ in vec3 positionShared;
 in vec2 texcoordShared;
 in vec3 normalShared;
 in mat4 viewShared;
+in vec3 eyeDirectionCamSpace;
 uniform sampler2D diffuse;
 
 layout(location = 0) out vec4 fragColor;
 
+const vec4 lightColour = vec4(1, 1, 1, 1);
+const float lightWattage = 150;
+const vec4 textureColour = texture2D(diffuse, texcoordShared);
+const vec3 cameraLightSource = (inverse(viewShared) * vec4(0, 0, 0, 1)).xyz;
+const vec3 ld = positionShared - cameraLightSource;
+const float distance = length(ld);
+const vec3 lightDirection = normalize(ld);
+
+vec4 getDiffuseComponent()
+{
+	float cosTheta = clamp(dot(normalShared, -lightDirection), 0, 1);
+	return textureColour*lightColour* lightWattage *cosTheta / (distance * distance);
+}
+
+vec4 getAmbientComponent()
+{
+	return textureColour * vec4(0.1, 0.1, 0.1, 1);
+}
+
+vec4 getSpecularComponent()
+{
+	vec3 E = normalize(eyeDirectionCamSpace);
+	vec3 R = reflect(-lightDirection, normalShared);
+	float cosAlpha = clamp(dot(E, R), 0, 1);
+	return textureColour * lightColour * lightWattage * pow(cosAlpha, 5) / (distance * distance);
+}
+
 void main()
 {
-	
-	vec3 lightDirection = vec3(0, 0, 0);
-	
-	vec4 lightColour = vec4(1, 1, 1, 1);
-	vec3 cameraLightSource = (inverse(viewShared) * vec4(0, 1, 0, 1)).xyz;
-	float lightWattage = 60;//600.0;
-	vec4 textureColour = texture2D(diffuse, texcoordShared);
-	
-	float diffuseComponent = clamp(dot(-lightDirection, normalShared), 0.1, 1.0); // this is costheta (clamped)
-	
-	fragColor = vec4(0, 0, 0, 0);
-	float distCam = length(positionShared - cameraLightSource);
-	fragColor += textureColour * lightColour * lightWattage * diffuseComponent / (distCam);//) * distCam);
-	//fragColor = textureColour;
+	fragColor = getAmbientComponent() + getDiffuseComponent() + getSpecularComponent();
 }
--- res/runtime/shaders/vanilla.vertex.glsl
+++ res/runtime/shaders/vanilla.vertex.glsl
@@ -9,6 +9,7 @@ out vec3 positionShared;
 out vec2 texcoordShared;
 out vec3 normalShared;
 out mat4 viewShared;
+out vec3 eyeDirectionCamSpace;
 
 uniform mat4 m;
 uniform mat4 v;
@@ -18,7 +19,9 @@ void main()
 {
 	gl_Position = (p * v * m) * vec4(position, 1.0);
 	positionShared = (m * vec4(position, 1.0)).xyz;
-	normalShared = (m * vec4(normal, 0.0)).xyz;
+	normalShared = normalize((m * vec4(normal, 0.0)).xyz);
 	texcoordShared = texcoord;
 	viewShared = v;
+	vec3 vertexPos_camSpace = (v * m * vec4(position, 1.0)).xyz;
+	eyeDirectionCamSpace = vec3(0,0,0) - vertexPos_camSpace;
 }
