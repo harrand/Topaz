a37b41e894a37528ac0fff6fa979a901ce063a0a
Harrand <harrysjh98@gmail.com>
2017-07-04 22:41:51 +0100

    Using C++17. Added include of <random> in utility.hpp (How did it ever compile without it?!)

--- qcpl_dll.bat
+++ qcpl_dll.bat
@@ -24,14 +24,14 @@ set lnkdir="%cpldir%\lnk"
 
 if not exist %lnkdir% mkdir %lnkdir%
 
-g++ -std=c++1z -Wall -pedantic-errors -O3 -shared -Wl,-no-undefined,--enable-runtime-pseudo-reloc,--out-implib,libtopazdll.a *.o -L%libdir% -lOpenGL32 -lSDL2 -lSDL2_mixer -lSDL2main -lmdl -o "topaz.dll"
+g++ -std=c++17 -Wall -pedantic-errors -O3 -shared -Wl,-no-undefined,--enable-runtime-pseudo-reloc,--out-implib,libtopazdll.a *.o -L%libdir% -lOpenGL32 -lSDL2 -lSDL2_mixer -lSDL2main -lmdl -o "topaz.dll"
 
 move "topaz.dll" %lnkdir%
 move "libtopazdll.a" %lnkdir%
 echo Linking Completed, "topaz.dll" is in %lnkdir%
 cd %scriptdir%
 cd src
-g++ -std=c++1z -Wall -Wextra -pedantic-errors -O3 -c test.cpp -I %incdir%
+g++ -std=c++17 -Wall -Wextra -pedantic-errors -O3 -c test.cpp -I %incdir%
 move test.o %lnkdir%
 color d
 echo Ensuring that dependencies are present...
@@ -39,7 +39,7 @@ cd "%scriptdir%\res\dep"
 xcopy /s "%cd%" %lnkdir%
 echo Dependencies copied...
 cd %lnkdir%
-g++ -O3 -static-libgcc -static-libstdc++ -o topaz_test_dependent.exe test.o "%scriptdir%\res\exe\topaz_test.res" -L. -L%libdir% -lOpenGL32 -lSDL2 -lSDL2_mixer -lSDL2main -lmdl -ltopaz
+g++ -O3 -o topaz_test_dependent.exe test.o "%scriptdir%\res\exe\topaz_test.res" -L. -L%libdir% -lOpenGL32 -lSDL2 -lSDL2_mixer -lSDL2main -lmdl -ltopaz
 move test.o "%cpldir%"
 move libtopazdll.a "%cpldir%"
 
--- src/utility.hpp
+++ src/utility.hpp
@@ -8,6 +8,7 @@
 #include <unordered_map>
 #include <limits>
 #include <typeinfo>
+#include <random>
 #include "vector.hpp"
 
 namespace CastUtility
