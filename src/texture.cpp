54eec85e527f7199e7c817e88332c884971bf610
Harry <harrysjh98@gmail.com>
2018-01-20 14:50:16 +0000

    Textures loaded from a file now receive linear mipmaps automatically.

--- src/texture.cpp
+++ src/texture.cpp
@@ -38,10 +38,11 @@ Texture::Texture(std::string filename, bool gamma_corrected, bool store_bitmap):
 	
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	
 	glTexImage2D(GL_TEXTURE_2D, 0, this->gamma_corrected ? GL_SRGB_ALPHA : GL_RGBA, this->width, this->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imgdata);
+	glGenerateMipmap(GL_TEXTURE_2D);
 	
 	if(store_bitmap)
 	{
