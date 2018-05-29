e170ce14ca88524c1af8ebc02351903f04c856f8
Harrand <harrysjh98@gmail.com>
2018-05-29 19:15:11 +0100

    GUI Label can now have a highlight colour.

--- src/gui_display.cpp
+++ src/gui_display.cpp
@@ -58,7 +58,7 @@ Texture* Panel::get_texture() const
         return nullptr;
 }
 
-Label::Label(Vector2<int> position_local_pixel_space, Font font, Vector3F text_colour, std::string text, GUI* parent, std::initializer_list<GUI*> children): GUI(position_local_pixel_space, {}, parent, children), font(font), text(text), text_colour(text_colour), mesh(tz::util::gui::gui_quad())
+Label::Label(Vector2<int> position_local_pixel_space, Font font, Vector3F text_colour, std::string text, std::optional<Vector3F> highlight_colour, GUI* parent, std::initializer_list<GUI*> children): GUI(position_local_pixel_space, {}, parent, children), font(font), text(text), text_colour(text_colour), highlight_colour(highlight_colour), mesh(tz::util::gui::gui_quad())
 {
     this->set_text(this->text);
 }
@@ -69,7 +69,8 @@ void Label::render(Shader& shader, int window_width_pixels, int window_height_pi
     shader.set_uniform<bool>("has_texture", true);
     shader.set_uniform<bool>("has_background_colour", true);
     shader.set_uniform<bool>("has_text_border_colour", false);
-    shader.set_uniform<Vector4F>("background_colour", {});
+    Vector3F background_rgb = this->highlight_colour.value_or(Vector3F{});
+    shader.set_uniform<Vector4F>("background_colour", {background_rgb, this->highlight_colour.has_value() ? 1.0f : 0.0f});
     Matrix4x4 projection = tz::transform::orthographic_projection(window_width_pixels, 0.0f, window_height_pixels, 0.0f, -1.0f, 1.0f);
     Matrix4x4 model = projection * tz::transform::model(Vector3F(this->get_x(), this->get_y() + this->get_height(), 0.0f), Vector3F(0, tz::consts::pi, tz::consts::pi), Vector3F(this->get_width(), this->get_height(), 0.0f));
     shader.set_uniform<Matrix4x4>("model_matrix", model);
@@ -80,16 +81,51 @@ void Label::render(Shader& shader, int window_width_pixels, int window_height_pi
     GUI::render(shader, window_width_pixels, window_height_pixels);
 }
 
+const std::string& Label::get_text() const
+{
+    return this->text;
+}
+
 void Label::set_text(std::string text)
 {
     this->text = text;
     this->update_texture();
 }
 
+const Vector3F& Label::get_text_colour() const
+{
+    return this->text_colour;
+}
+
 void Label::set_text_colour(Vector3F text_colour)
 {
     this->text_colour = text_colour;
     this->update_texture();
+}
+
+bool Label::has_highlight() const
+{
+    return this->highlight_colour.has_value();
+}
+
+const std::optional<Vector3F>& Label::get_highlight_colour() const
+{
+    return this->highlight_colour;
+}
+
+void Label::set_highlight_colour(Vector3F highlight_colour)
+{
+    this->highlight_colour = highlight_colour;
+}
+
+void Label::remove_highlight()
+{
+    this->highlight_colour = std::nullopt;
+}
+
+const Font& Label::get_font() const
+{
+    return this->font;
 }
 
 void Label::set_font(Font font)
--- src/gui_display.hpp
+++ src/gui_display.hpp
@@ -22,16 +22,24 @@ private:
 class Label : public GUI
 {
 public:
-    Label(Vector2<int> position_local_pixel_space, Font font, Vector3F text_colour, std::string text, GUI* parent = nullptr, std::initializer_list<GUI*> children = {});
+    Label(Vector2<int> position_local_pixel_space, Font font, Vector3F text_colour, std::string text, std::optional<Vector3F> highlight_colour = {}, GUI* parent = nullptr, std::initializer_list<GUI*> children = {});
     virtual void render(Shader& shader, int window_width_pixels, int window_height_pixels) const override;
+    const std::string& get_text() const;
     void set_text(std::string text);
+    const Vector3F& get_text_colour() const;
     void set_text_colour(Vector3F text_colour);
+    bool has_highlight() const;
+    const std::optional<Vector3F>& get_highlight_colour() const;
+    void set_highlight_colour(Vector3F highlight_colour);
+    void remove_highlight();
+    const Font& get_font() const;
     void set_font(Font font);
 private:
     void update_texture();
     Font font;
     std::string text;
     Vector3F text_colour;
+    std::optional<Vector3F> highlight_colour;
     std::unique_ptr<Texture> text_render_texture;
     Mesh mesh;
 };
