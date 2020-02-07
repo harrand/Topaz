f98e18bd3e8f3a98fa2fe5573c1747423fafe150
Harrand <harrysjh98@gmail.com>
2020-02-07 22:05:19 +0000

    Revert "+ Added Managed Buffers, which currently only work for terminal buffers. Also added a test for it. Due to the requirement of keeping regions ordered by how close they are to the mapping beginning, the struct is now super inefficient and probably needs to be rewritten. But the general idea is there. Good luck future me!"
    This reverts commit 54ed031b3d787e259607d9aad9ea71d3482511a1.

--- src/memory/block.hpp
+++ src/memory/block.hpp
@@ -16,15 +16,10 @@ namespace tz::mem
         Block(void* begin, void* end);
         Block(void* begin, std::size_t size);
         std::size_t size() const;
-        template<typename T = char>
-        T* get(std::size_t idx);
-        template<typename T = char>
-        const T* get(std::size_t idx) const;
 
         void* begin;
         void* end;
     };
 }
 
-#include "memory/block.inl"
 #endif // TOPAZ_CONTIGUOUS_BLOCK_HPP
