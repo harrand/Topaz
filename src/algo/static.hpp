ef67663272ae95fedbe3d98cfcce676634cbfbd7
Harrand <harrysjh98@gmail.com>
2020-09-13 16:48:40 +0100

    * Documentation pass for tz::algo static utility

--- src/algo/static.hpp
+++ src/algo/static.hpp
@@ -5,7 +5,20 @@
 
 namespace tz::algo
 {
+	/**
+	* \addtogroup tz_algo Topaz Algorithms Library (tz::algo)
+	* Contains common algorithms used in Topaz modules that aren't available in the C++ standard library.
+	* @{
+	*/
 
+	/**
+	 * Invoke the given functor with a single std::size_t parameter End - Begin times, incrementing the parameter each time.
+	 * This should be used to iterate at compile-time.
+	 * @tparam Begin Value at first iteration.
+	 * @tparam End Value at last iteration.
+	 * @tparam Functor Functor type to invoke. Must be of the signature X(std::size_t) where X is any type. This is recommended to be void as the return value will always be discarded.
+	 * @param function Specific function to invoke. It's signature must match the tparam Functor.
+	 */
 	template<std::size_t Begin, std::size_t End, typename Functor>
 	constexpr void static_for(const Functor& function)
 	{
@@ -16,6 +29,12 @@ namespace tz::algo
 		}
 	}
 
+	/**
+	 * Check if a Needle is found in a Haystack at compile-time.
+	 * @tparam Needle Needle type to check exists within Haystack...
+	 * @tparam Haystack Parameter pack which may or may not contain the type Needle.
+	 * @return True if the template parameter pack 'Haystack' contains a type such that std::is_same<Needle, Type> is true. Otherwise, returns false.
+	 */
 	template<typename Needle, typename... Haystack>
 	constexpr bool static_find()
 	{
@@ -30,6 +49,11 @@ namespace tz::algo
 		return b;
 	}
 
+	/**
+	 * Query as to whether the given type is move-constructible and move-assignable.
+	 * @tparam T Type to query.
+	 * @return True if T is move-constructible and move-assignable.
+	 */
 	template<class T>
 	constexpr bool moveable()
 	{
@@ -37,12 +61,21 @@ namespace tz::algo
 			&& std::is_move_assignable_v<T>;
 	}
 
+	/**
+	 * Query as to whether the given type is copy-constructible and copt-assignable.
+	 * @tparam T Type to query.
+	 * @return True if T is copy-constructible and copy-assignable.
+	 */
 	template<class T>
 	constexpr bool copyable()
 	{
 		return std::is_copy_constructible_v<T>
 			&& std::is_copy_assignable_v<T>;
 	}
+
+	/**
+	 * @}
+	 */
 }
 
 #endif // TOPAZ_ALGO_STATIC_HPP
