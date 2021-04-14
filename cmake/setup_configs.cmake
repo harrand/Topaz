60b975093264c1e17ef6d9cf082709d635975fe6
Harrand <harrysjh98@gmail.com>
2021-04-14 01:01:43 +0100

    + Added helper cmake script to reduce the size of top-level CMakeLists.txt. Also tz_vk now conditionally links against vulkan/glad depending on what build config is chosen

---
+++ cmake/setup_configs.cmake
@@ -0,0 +1,24 @@
+function(configure_opengl target)
+    target_compile_definitions(${target} PUBLIC -DTZ_VULKAN=0 -DTZ_OGL=1)
+    
+    target_link_libraries(${target} PUBLIC glad)
+endfunction()
+
+function(configure_vulkan target)
+    target_compile_definitions(${target} PUBLIC -DTZ_VULKAN=1 -DTZ_OGL=0)
+
+    message(STATUS "configure_vulkan(${target}): $VK_SDK_PATH == $ENV{VK_SDK_PATH}")
+    target_link_directories(${target} PRIVATE "$ENV{VK_SDK_PATH}/Lib")
+    target_include_directories(${target} PRIVATE "$ENV{VK_SDK_PATH}/include")
+    target_link_libraries(${target} PRIVATE vulkan-1)
+endfunction()
+
+function(configure_debug target)
+    target_compile_definitions(${target} PUBLIC -DTZ_DEBUG=1)
+    target_compile_options(${target} PRIVATE -g)
+endfunction()
+
+function(configure_release target)
+    target_compile_definitions(${target} PUBLIC -DTZ_DEBUG=0)
+    target_compile_options(${target} PRIVATE -O3)
+endfunction()
