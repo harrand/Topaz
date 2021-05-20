d465ea077b5dd7c20e4c61e1a508541b4162b89a
Harrand <harrysjh98@gmail.com>
2021-05-21 00:40:25 +0100

    * tz_mesh_test now checks indices aswell as vertices

--- test/gl/mesh_test.cpp
+++ test/gl/mesh_test.cpp
@@ -18,10 +18,18 @@ int main()
             0, 1, 2
         };
         tz::gl::MeshInput mesh_input{mesh};
-        const std::size_t expected_vertices_size = mesh.vertices.length() * sizeof(tz::gl::Vertex);
-        const std::size_t a = mesh_input.get_vertex_bytes().size();
-        const std::size_t b = mesh_input.get_vertex_bytes().size_bytes();
-        tz_assert(a == b && b == expected_vertices_size, "Unexpected MeshInput dimensions");
+
+        {
+            const std::size_t expected_vertices_size = mesh.vertices.length() * sizeof(tz::gl::Vertex);
+            const std::size_t a = mesh_input.get_vertex_bytes().size();
+            const std::size_t b = mesh_input.get_vertex_bytes().size_bytes();
+            tz_assert(a == b && b == expected_vertices_size, "Unexpected MeshInput Vertices Dimensions");
+        }
+        {
+            const std::size_t expected_indices_size = mesh.indices.length();
+            const std::size_t expected_indices_bytes = mesh.indices.length() * sizeof(unsigned int);
+            tz_assert(mesh_input.get_indices().size() == expected_indices_size && mesh_input.get_indices().size_bytes() == expected_indices_bytes, "Unexpected MeshInput Indices Dimensions");
+        }
     }
     tz::terminate();
 }
