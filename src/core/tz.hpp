78478c8d03b803c6851272d0c907d5f57faebcd3
Harrand <harrysjh98@gmail.com>
2021-05-22 03:24:02 +0100

    * Documentation pass for tz.hpp

--- src/core/tz.hpp
+++ src/core/tz.hpp
@@ -10,6 +10,9 @@ namespace tz
 	 * A collection of platform-agnostic core interfaces.
 	 * @{
 	 */
+    /**
+     * Describes a given application.
+     */
     enum class ApplicationType
     {
         WindowApplication,
@@ -17,9 +20,35 @@ namespace tz
         Headless
     };
 
+    /**
+     * @brief Initialise Topaz.
+     * @pre If anything in `tz` has been invoked before this, then the behaviour of the program is undefined.
+     * @post If `tz::terminate()` is not called before the end of program runtime, then the behaviour of the program is undefined.
+     * @details This initialises the render-api backend. Also creates a window unless `ApplicationType::Headless` is specified. Note that for `ApplicationType::HiddenWindowApplication`, a window is created but it is invisible.
+     * 
+     * @param game_info Information about the application
+     * @param app_type Describes the anatomy of the application.
+     */
     void initialise(GameInfo game_info, ApplicationType app_type = ApplicationType::WindowApplication);
+    /**
+     * @brief Terminate Topaz.
+     * @pre If `tz::initialise` has not been invoked before this, then the behaviour of the program is undefined.
+     * @post If anything in `tz` is invoked after this, then the behaviour of the program is undefined.
+     * @details This destroys the render-api backend. If a window was created previously by `tz::initialise`, then it will be destroyed here.
+     */
     void terminate();
+    /**
+     * @brief Retrieve the application window.
+     * @pre `tz::initialise` has been invoked
+     * @post `tz::terminate()` has not yet been invoked.
+     * @return Window& Reference to the application window.
+     */
     Window& window();
+    /**
+     * @brief Query as to whether Topaz is initialised.
+     * 
+     * @return true If `tz::initialise` has been invoked and `tz::terminate()` has not yet been invoked. Otherwise, returns false.
+     */
     bool is_initialised();
     
     /**
