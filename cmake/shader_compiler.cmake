79e81424443554aeee19edbe350eb4485ca28d2c
Harrand <harrysjh98@gmail.com>
2021-06-02 17:41:24 +0100

    * Shaders within the CMake process now by default run on tzslc with -mall for both OpenGL and Vulkan. All behaviour is unchanged

--- cmake/shader_compiler.cmake
+++ cmake/shader_compiler.cmake
@@ -6,38 +6,49 @@ macro(add_shader_vulkan)
     set(VK_GLSLC_EXECUTABLE_PATH ${VK_BIN_DIR}/glslc.exe)
 
     set(shader_path ${PROJECT_SOURCE_DIR}/${SHADER})
+    set(processed_shader_path ${PROJECT_BINARY_DIR}/${SHADER}.glsl)
+
+    add_custom_command(
+        OUTPUT ${processed_shader_path}
+        COMMENT "TZSLC_VK: Building ${processed_shader_path}"
+        COMMAND "${TZSLC_EXECUTABLE_PATH}" ${shader_path} -mall -o ${processed_shader_path}
+        DEPENDS ${tzslc} ${shader_path}
+        IMPLICIT_DEPENDS CXX ${shader_path}
+        VERBATIM
+    )
     set(spv_path ${PROJECT_BINARY_DIR}/${SHADER}.spv)
 
     add_custom_command(
         OUTPUT ${spv_path}
         COMMENT "VK: Building ${spv_path}"
-        COMMAND ${VK_GLSLC_EXECUTABLE_PATH} -o ${spv_path} ${shader_path}
-        DEPENDS ${shader_path}
-        IMPLICIT_DEPENDS CXX ${shader_path}
+        COMMAND ${VK_GLSLC_EXECUTABLE_PATH} -o ${spv_path} ${processed_shader_path}
+        DEPENDS ${processed_shader_path}
         VERBATIM)
 
     set_source_files_properties(${spv_path} PROPERTIES GENERATED TRUE)
+    add_dependencies(${ADD_SHADER_TARGET} tzslc)
     target_sources(${ADD_SHADER_TARGET} PRIVATE ${spv_path})
 endmacro()
 
 macro(add_shader_opengl)
     set(shader_path ${PROJECT_SOURCE_DIR}/${SHADER})
-    set(output_path ${PROJECT_BINARY_DIR}/${SHADER})
+    set(output_path ${PROJECT_BINARY_DIR}/${SHADER}.glsl)
 
     add_custom_command(
         OUTPUT ${output_path}
-        COMMENT "OGL: Building ${output_path}"
-        COMMAND ${CMAKE_COMMAND} -E copy ${shader_path} ${output_path}
-        IMPLICIT_DEPENDS CXX ${shader_path}
+        COMMENT "TZSLC_OGL: Building ${output_path}"
+        COMMAND "${TZSLC_EXECUTABLE_PATH}" ${shader_path} -mall -o ${output_path}
+        DEPENDS ${tzslc} ${shader_path}
         VERBATIM
     )
 
     set_source_files_properties(${output_path} PROPERTIES GENERATED TRUE)
+    add_dependencies(${ADD_SHADER_TARGET} tzslc)
     target_sources(${ADD_SHADER_TARGET} PRIVATE ${output_path})
 endmacro()
 
 function(add_shader)
-    
+    set(TZSLC_EXECUTABLE_PATH ${PROJECT_BINARY_DIR}/tools/tzslc/tzslc)
     cmake_parse_arguments(
         ADD_SHADER
         ""
--- tools/tzslc/tzslc_main.cpp
+++ tools/tzslc/tzslc_main.cpp
@@ -30,12 +30,28 @@ PreprocessorModuleField selected_modules(int argc, char** argv)
     return modules;
 }
 
+FILE* get_output_stream(int argc, char** argv)
+{
+    FILE* output = stdout;
+    for(std::size_t i = 0; i < argc - 1; i++)
+    {
+        std::string_view arg{argv[i]};
+        std::string_view arg_next{argv[i + 1]};
+        if(arg == "-o")
+        {
+            output = fopen(arg_next.data(), "w");
+        }
+    }
+    return output;
+}
+
 int main(int argc, char** argv)
 {
     tz_assert(argc >= 2, "Not enough arguments (%d). At least 2", argc);
     const char* glsl_filename = argv[1];
     PreprocessorModuleField modules = selected_modules(argc - 2, argv + 2);
 
+    FILE* out = get_output_stream(argc, argv);
     {
         std::ifstream shader{glsl_filename, std::ios::ate | std::ios::binary};
         tz_assert(shader.is_open(), "Cannot open shader file %s", glsl_filename);
@@ -47,6 +63,10 @@ int main(int argc, char** argv)
         shader.close();
 
         tzslc::preprocess(modules, buffer);
-        std::printf("%s", buffer.data());
+        std::fprintf(out, "%s", buffer.data());
+    }
+    if(out != stdout)
+    {
+        fclose(out);
     }
 }
