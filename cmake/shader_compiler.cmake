8c376d150e8c14e70907dcc6f72f6b2bec186e29
Harrand <harrysjh98@gmail.com>
2021-06-03 00:08:08 +0100

    Clarified cmake printouts during tzslc invocations

--- cmake/shader_compiler.cmake
+++ cmake/shader_compiler.cmake
@@ -6,46 +6,47 @@ macro(add_shader_vulkan)
     set(VK_GLSLC_EXECUTABLE_PATH ${VK_BIN_DIR}/glslc.exe)
 
     set(shader_path ${PROJECT_SOURCE_DIR}/${SHADER})
-    set(processed_shader_path ${PROJECT_BINARY_DIR}/${SHADER}.glsl)
+    set(processed_shader_name ${SHADER}.glsl)
+    set(processed_shader_path ${PROJECT_BINARY_DIR}/${processed_shader_name})
 
     add_custom_command(
         OUTPUT ${processed_shader_path}
-        COMMENT "TZSLC_VK: Building ${processed_shader_path}"
+        COMMENT "TZSLC_VK: Preprocessing ${SHADER} -> ${processed_shader_name}"
         COMMAND "${TZSLC_EXECUTABLE_PATH}" ${shader_path} -mall -o ${processed_shader_path}
-        DEPENDS ${tzslc} ${shader_path}
+        DEPENDS tzslc ${shader_path}
         IMPLICIT_DEPENDS CXX ${shader_path}
         VERBATIM
     )
-    set(spv_path ${PROJECT_BINARY_DIR}/${SHADER}.spv)
+    set(spv_name ${SHADER}.spv)
+    set(spv_path ${PROJECT_BINARY_DIR}/${spv_name})
 
     add_custom_command(
         OUTPUT ${spv_path}
-        COMMENT "VK: Building ${spv_path}"
+        COMMENT "TZSLC_VK: Building ${processed_shader_name} -> ${spv_name}"
         COMMAND ${VK_GLSLC_EXECUTABLE_PATH} -o ${spv_path} ${processed_shader_path}
-        DEPENDS ${processed_shader_path}
+        DEPENDS tzslc ${processed_shader_path}
         IMPLICIT_DEPENDS CXX ${processed_shader_path}
         VERBATIM)
 
     set_source_files_properties(${processed_shader_path} ${spv_path} PROPERTIES GENERATED TRUE)
-    add_dependencies(${ADD_SHADER_TARGET} tzslc)
     target_sources(${ADD_SHADER_TARGET} PRIVATE ${processed_shader_path} ${spv_path})
 endmacro()
 
 macro(add_shader_opengl)
     set(shader_path ${PROJECT_SOURCE_DIR}/${SHADER})
-    set(output_path ${PROJECT_BINARY_DIR}/${SHADER}.glsl)
+    set(output_name ${SHADER}.glsl)
+    set(output_path ${PROJECT_BINARY_DIR}/${output_name}})
 
     add_custom_command(
         OUTPUT ${output_path}
-        COMMENT "TZSLC_OGL: Building ${output_path}"
+        COMMENT "TZSLC_OGL: Building ${SHADER} -> ${output_name}"
         COMMAND "${TZSLC_EXECUTABLE_PATH}" ${shader_path} -mall -o ${output_path}
-        DEPENDS ${shader_path}
+        DEPENDS tzslc ${shader_path}
         IMPLICIT_DEPENDS CXX ${processed_shader_path}
         VERBATIM
     )
 
     set_source_files_properties(${output_path} PROPERTIES GENERATED TRUE)
-    add_dependencies(${ADD_SHADER_TARGET} tzslc)
     target_sources(${ADD_SHADER_TARGET} PRIVATE ${output_path})
 endmacro()
 
