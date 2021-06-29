568fa5474e9f2b15169783aefe38b191599f43d2
Harrand <harrysjh98@gmail.com>
2021-06-29 23:54:38 +0100

    * Minor documentation pass

--- src/gl/buffer.hpp
+++ src/gl/buffer.hpp
@@ -8,9 +8,23 @@ namespace tz::gl
 {
     struct BufferData
     {
+        /**
+         * @brief Retrieve buffer data from an array.
+         * 
+         * @tparam T Array value type.
+         * @param data View into the array to copy from.
+         * @return BufferData Containing the buffer data.
+         */
         template<typename T>
         static BufferData from_array(std::span<const T> data);
 
+        /**
+         * @brief Retrieve buffer data from a single value.
+         * 
+         * @tparam T Value type.
+         * @param data Value to represent the buffer data.
+         * @return BufferData Containing the buffer data.
+         */
         template<typename T>
         static BufferData from_value(const T& data);
         std::vector<std::byte> data;
