8538f77965e2385e4995487acbbcbbb7473bbde6
Harrand <harrysjh98@gmail.com>
2021-08-29 23:15:22 +0100

    - Removed renderdoc crap that shouldn't be on master branch. Also tz_compute_demo now looks exactly like tz_triangle_demo but its still doing its compute work (albeit under the hood and no way of seeing the results yet)

--- demo/gl/tz_compute_demo.cpp
+++ demo/gl/tz_compute_demo.cpp
@@ -7,10 +7,6 @@
 #include "gl/input.hpp"
 #include "gl/shader.hpp"
 
-// TEMP
-#include "renderdoc_app.h"
-#include <windows.h>
-
 float get_aspect_ratio()
 {
     return tz::window().get_width() / tz::window().get_height();
@@ -18,17 +14,6 @@ float get_aspect_ratio()
 
 int main()
 {
-    RENDERDOC_API_1_1_2 *rdoc_api = NULL;
-
-    // At init, on windows
-    if(HMODULE mod = GetModuleHandleA("renderdoc.dll"))
-    {
-        pRENDERDOC_GetAPI RENDERDOC_GetAPI =
-            (pRENDERDOC_GetAPI)GetProcAddress(mod, "RENDERDOC_GetAPI");
-        int ret = RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_1_2, (void **)&rdoc_api);
-        tz_assert(ret == 1, "RenderDoc error");
-    }
-
     tz::WindowInitArgs wargs = tz::default_args;
     wargs.resizeable = false;
     tz::initialise({"tz_compute_demo", tz::Version{1, 0, 0}, tz::info()}, tz::ApplicationType::WindowApplication, wargs);
@@ -42,28 +27,54 @@ int main()
         tz::gl::Shader shader = device.create_shader(shader_builder);
 
         tz::gl::ProcessorBuilder processor_builder;
+        tz::gl::BufferResource compute_buf{tz::gl::BufferData::from_array<tz::Mat4>
+        ({{
+            tz::model({0.0f, 0.0f, -1.0f}, {0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}),
+            tz::view({0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}),
+            tz::perspective(1.27f, get_aspect_ratio(), 0.1f, 1000.0f)
+        }})};
+        processor_builder.add_resource(compute_buf);
+        processor_builder.set_shader(shader);
+        tz::gl::Processor processor = device.create_processor(processor_builder);
+
+        // Render something placeholder to the screen. Not necessary but otherwise may yield output differences between VK/OGL versions
+        tz::gl::ShaderBuilder render_shader_builder;
+        render_shader_builder.set_shader_file(tz::gl::ShaderType::VertexShader, ".\\demo\\gl\\triangle_demo.vertex.tzsl");
+        render_shader_builder.set_shader_file(tz::gl::ShaderType::FragmentShader, ".\\demo\\gl\\triangle_demo.fragment.tzsl");
+
+        tz::gl::Shader render_shader = device.create_shader(render_shader_builder);
+
+        tz::gl::RendererBuilder renderer_builder;
+        tz::gl::Mesh mesh;
+        mesh.vertices =
+            {
+                tz::gl::Vertex{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f}, {}, {}, {}},
+                tz::gl::Vertex{{0.5f, -0.5f, 0.0f}, {0.0f, 0.0f}, {}, {}, {}},
+                tz::gl::Vertex{{0.5f, 0.5f, 0.0f}, {0.0f, 1.0f}, {}, {}, {}}
+            };
+        mesh.indices = {0, 1, 2};
+        tz::gl::MeshInput mesh_input{mesh};
+        // Note: Window is resizeable but we don't amend the aspect-ratio if it does. This is for simplicity's sake -- This is done properly in tz_dynamic_triangle_demo.
         tz::gl::BufferResource buf_res{tz::gl::BufferData::from_array<tz::Mat4>
         ({{
             tz::model({0.0f, 0.0f, -1.0f}, {0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}),
             tz::view({0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}),
             tz::perspective(1.27f, get_aspect_ratio(), 0.1f, 1000.0f)
         }})};
-        processor_builder.add_resource(buf_res);
-        processor_builder.set_shader(shader);
-        tz::gl::Processor processor = device.create_processor(processor_builder);
+
+        renderer_builder.add_input(mesh_input);
+        renderer_builder.set_output(tz::window());
+        renderer_builder.add_resource(buf_res);
+        renderer_builder.set_pass(tz::gl::RenderPassAttachment::Colour);
+        renderer_builder.set_shader(render_shader);
+        tz::gl::Renderer renderer = device.create_renderer(renderer_builder);
+        renderer.set_clear_colour({0.1f, 0.2f, 0.4f, 1.0f});
 
         while(!tz::window().is_close_requested())
         {
-            if(rdoc_api)
-            {
-                rdoc_api->StartFrameCapture(nullptr, nullptr);
-            }
             tz::window().update();
             processor.process();
-            if(rdoc_api)
-            {
-                rdoc_api->EndFrameCapture(nullptr, nullptr);
-            }
+            renderer.render();
         }
     }
     tz::terminate();
