5811dfafe955c9100064ccefe010985eea8e4b6a
Harrand <harrysjh98@gmail.com>
2022-02-28 23:46:35 +0000

    * Slightly more for tz_renderer_test

--- test/gl/tz_renderer_test.cpp
+++ test/gl/tz_renderer_test.cpp
@@ -2,6 +2,7 @@
 #include "gl/imported_shaders.hpp"
 #include "gl/device.hpp"
 #include "gl/renderer.hpp"
+#include "gl/resource.hpp"
 
 #include ImportedShaderHeader(empty, vertex)
 #include ImportedShaderHeader(empty, fragment)
@@ -12,7 +13,26 @@ void empty_renderer(tz::gl2::Device& dev)
 	rinfo.shader().set_shader(tz::gl2::ShaderStage::Vertex, ImportedShaderSource(empty, vertex));
 	rinfo.shader().set_shader(tz::gl2::ShaderStage::Fragment, ImportedShaderSource(empty, fragment));
 	tz::gl2::Renderer empty = dev.create_renderer(rinfo);
+	empty.render();
+}
 
+void renderer_creation(tz::gl2::Device& dev)
+{
+	tz::gl2::ImageResource ires0 = tz::gl2::ImageResource::from_uninitialised(tz::gl2::ImageFormat::RGBA32, {1u, 1u});
+	tz::gl2::BufferResource bres0 = tz::gl2::BufferResource::from_one(5.0f);
+
+	tz::gl2::RendererInfo rinfo1;
+	rinfo1.shader().set_shader(tz::gl2::ShaderStage::Vertex, ImportedShaderSource(empty, vertex));
+	rinfo1.shader().set_shader(tz::gl2::ShaderStage::Fragment, ImportedShaderSource(empty, fragment));
+	rinfo1.add_resource(ires0);
+	tz::gl2::Renderer renderer1 = dev.create_renderer(rinfo1);
+
+	tz::gl2::RendererInfo rinfo2 = rinfo1;
+	rinfo2.add_resource(bres0);
+	tz::gl2::Renderer renderer2 = dev.create_renderer(rinfo2);
+
+	renderer1.render();
+	renderer2.render();
 }
 
 int main()
@@ -25,6 +45,7 @@ int main()
 	{
 		tz::gl2::Device dev;
 		empty_renderer(dev);
+		renderer_creation(dev);
 	}
 	tz::terminate();
 }
