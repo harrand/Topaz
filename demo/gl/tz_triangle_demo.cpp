c521d435f7a45decdfd2867270c20443558db8ff
Harrand <harrysjh98@gmail.com>
2022-03-05 00:13:36 +0000

    * tz_triangle_demo massively simplified

--- demo/gl/tz_triangle_demo.cpp
+++ demo/gl/tz_triangle_demo.cpp
@@ -3,7 +3,6 @@
 #include "core/profiling/zone.hpp"
 #include "gl/device.hpp"
 #include "gl/renderer.hpp"
-#include "gl/resource.hpp"
 #include "gl/imported_shaders.hpp"
 
 #include ImportedShaderHeader(tz_triangle_demo, vertex)
@@ -14,59 +13,13 @@ int main()
 	tz::initialise
 	({
 		.name = "tz_triangle_demo",
-		.window = {.flags = {.resizeable = false}}
 	});
 	{
 		tz::gl2::Device dev;
 
-		using namespace tz::literals;
-		tz::gl2::ImageResource img = tz::gl2::ImageResource::from_memory
-		(
-			tz::gl2::ImageFormat::RGBA32,
-			{2u, 2u},
-			{
-				0b0000'0000_uc,
-				0b0000'0000_uc,
-				0b1111'1111_uc,
-				0b1111'1111_uc,
-
-				0b1111'1111_uc,
-				0b0000'0000_uc,
-				0b0000'0000_uc,
-				0b1111'1111_uc,
-
-				0b0000'0000_uc,
-				0b1111'1111_uc,
-				0b0000'0000_uc,
-				0b1111'1111_uc,
-
-				0b0000'0000_uc,
-				0b0000'0000_uc,
-				0b1111'1111_uc,
-				0b1111'1111_uc
-			}
-		);
-
-		struct TriangleVertexData
-		{
-			tz::Vec3 position;
-			float pad0;
-			tz::Vec2 texcoord;
-			float pad1[2];
-		};
-
-		tz::gl2::BufferResource buf = tz::gl2::BufferResource::from_many
-		({
-			TriangleVertexData{.position = {-0.5f, -0.5f, 0.0f}, .texcoord = {0.0f, 0.0f}},
-			TriangleVertexData{.position = {0.0f, 0.5f, 0.0f}, .texcoord = {0.5f, 1.0f}},
-			TriangleVertexData{.position = {0.5f, -0.5f, 0.0f}, .texcoord = {1.0f, 0.0f}},
-		});
-
 		tz::gl2::RendererInfo rinfo;
 		rinfo.shader().set_shader(tz::gl2::ShaderStage::Vertex, ImportedShaderSource(tz_triangle_demo, vertex));
 		rinfo.shader().set_shader(tz::gl2::ShaderStage::Fragment, ImportedShaderSource(tz_triangle_demo, fragment));
-		rinfo.add_resource(img);
-		rinfo.add_resource(buf);
 		rinfo.set_options({tz::gl2::RendererOption::NoDepthTesting});
 
 		tz::gl2::Renderer renderer = dev.create_renderer(rinfo);
