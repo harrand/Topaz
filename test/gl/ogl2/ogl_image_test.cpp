0b4333118b9166d47360c34d64ff49117630a778
Harrand <harrysjh98@gmail.com>
2022-03-14 02:22:00 +0100

    * ogl_image_test no longer tries to do any bindless texture stuff

--- test/gl/ogl2/ogl_image_test.cpp
+++ test/gl/ogl2/ogl_image_test.cpp
@@ -21,27 +21,6 @@ void create_empty_image()
 	}};
 }
 
-void create_empty_bindless_image()
-{
-	using namespace tz::gl::ogl2;
-	Image image
-	{{
-		.format = ImageFormat::RGBA32,
-		.dimensions = {1u, 1u},
-		.sampler =
-		{
-			.min_filter = LookupFilter::Nearest,
-			.mag_filter = LookupFilter::Nearest,
-			.address_mode_s = AddressMode::ClampToEdge,
-			.address_mode_t = AddressMode::ClampToEdge,
-			.address_mode_r = AddressMode::ClampToEdge
-		}
-	}};
-	tz_assert(!image.is_bindless(), "Image was wrongly considered to be bindless when it really shouldn't (this is a major hazard, fix this asap)");
-	image.make_bindless();
-	tz_assert(image.is_bindless(), "Image was wrongly considered NOT to be bindless when it really should be.");
-}
-
 int main()
 {
 	tz::initialise
@@ -51,7 +30,6 @@ int main()
 	});
 	{
 		create_empty_image();
-		create_empty_bindless_image();
 	}
 	tz::terminate();
 	return 0;
