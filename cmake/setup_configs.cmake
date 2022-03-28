7b10bfb6eeaf41bc08a2934ec471e9c01e9903df
Harrand <harrysjh98@gmail.com>
2022-03-28 02:21:41 +0100

    * Small refactoring in CMakeLists.txt, reducing the size of the top-level somewhat

--- cmake/setup_configs.cmake
+++ cmake/setup_configs.cmake
@@ -1,3 +1,20 @@
+function(configure_common target)
+	if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+		# GCC/Clang options
+		target_compile_options(${target} PRIVATE -Wall -Wextra -Wdelete-non-virtual-dtor -pedantic-errors)
+		if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+			# Clang-only options
+			# Use GNU's `, __VA_ARGS__` extension (used for tz_assert, tz_error, tz_report)
+			target_compile_options(${target} PRIVATE -Wno-gnu-zero-variadic-macro-arguments)
+		elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+			# GCC-only options
+			target_compile_options(${target} PRIVATE -Wshadow=local)
+		endif()
+	elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+		# MSVC-only options
+	endif()
+endfunction()
+
 function(configure_opengl target)
 	target_compile_definitions(${target} PUBLIC -DTZ_VULKAN=0 -DTZ_OGL=1)
 	
