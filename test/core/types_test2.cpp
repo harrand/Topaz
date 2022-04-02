c65e4d072a3c441e5838f993120b86c46af05bca
Harrand <harrysjh98@gmail.com>
2022-04-02 16:47:16 +0100

    + Added a second unit test for core/types.hpp

---
+++ test/core/types_test2.cpp
@@ -0,0 +1,45 @@
+#include "core/types.hpp"
+
+constexpr void traits();
+
+int main()
+{
+	traits();
+	return 0;
+}
+
+double fimpl0(int){return 0.0;}
+void fimpl1(){}
+
+constexpr void traits()
+{
+	static_assert(tz::Arithmetic<int>);
+	static_assert(tz::Arithmetic<float>);
+	static_assert(tz::Arithmetic<char>);
+	static_assert(tz::Arithmetic<bool>);
+	static_assert(!tz::Arithmetic<int*>);
+
+	static_assert(tz::ConstType<const int>);
+	static_assert(!tz::ConstType<int>);
+	static_assert(!tz::ConstType<const int*>);
+	static_assert(tz::ConstType<const int* const>);
+
+	enum class EC{};
+	enum E{};
+	static_assert(!tz::EnumClass<E>);
+	static_assert(tz::EnumClass<EC>);
+
+	static_assert(tz::Number<int>);
+	static_assert(tz::Number<float>);
+	static_assert(!tz::Number<int*>);
+	static_assert(!tz::Number<bool>);
+	static_assert(!tz::Number<char>);
+
+	constexpr auto lambda0 = [](int x)constexpr->double{return x * 2;};
+	static_assert(tz::Function<decltype(lambda0), double, int>);
+	static_assert(tz::Function<decltype(fimpl0), double, int>);
+
+	constexpr auto lambda1 = [](){};
+	static_assert(tz::Action<decltype(lambda1)>);
+	static_assert(tz::Action<decltype(fimpl1)>);
+}
