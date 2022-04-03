49e7e486616096f2c6a41f744b63c4e73367e99e
Harrand <harrysjh98@gmail.com>
2022-04-03 13:49:53 +0100

    * Documentation pass for tz::phys::Body and a tiny bit more testing

--- src/phys/body.hpp
+++ src/phys/body.hpp
@@ -10,12 +10,30 @@ namespace tz::phys
 	 */
 	struct Body
 	{
+		/**
+		 * Location of the body in the world. Defaults to origin, with no rotation and unit scale.
+		 */
 		Transform transform = {};
+		/**
+		 * Velocity. Defaults to {0, 0, 0}.
+		 */
 		tz::Vec3 velocity{0.0f, 0.0f, 0.0f};
+		/**
+		 * Acceleration. Defaults to {0, 0, 0}.
+		 */
 		tz::Vec3 acceleration{0.0f, 0.0f, 0.0f};
 
+		/**
+		 * Update body by performing motion integration.
+		 */
 		void update(float delta_millis);
+		/**
+		 * Retrieve the current speed of the body.
+		 */
 		float get_speed() const;
+		/**
+		 * Retrieve the length of the current acceleration of the body.
+		 */
 		float get_acceleration_magnitude() const;
 	};
 }
--- test/phys/tz_body_test.cpp
+++ test/phys/tz_body_test.cpp
@@ -10,7 +10,18 @@ int main()
 
 void scalar_gets()
 {
-	constexpr tz::phys::Body b;
+	constexpr tz::Vec3 zero{0.0f, 0.0f, 0.0f};
+	tz::phys::Body b;
 	tz_assert(b.get_speed() == 0, "Default body has nonzero speed %.2f m/s", b.get_speed());
 	tz_assert(b.get_acceleration_magnitude() == 0, "Default body has nonzero acceleration %.2f m/s/s", b.get_acceleration_magnitude());
+
+	b.velocity = {0.0f, -9.81f, 0.0f};
+	tz_assert(b.get_speed() == 9.81f, "Body speed is calculated wrongly. Expected %.2f, got %.2f", 9.81f, b.get_speed());
+	tz_assert(b.transform.position == zero, "Default body is not at origin. Expected {%.2f, %.2f, %.2f}, got {%.2f, %.2f, %.2f}", 0.0f, 0.0f, 0.0f, b.transform.position[0], b.transform.position[1], b.transform.position[2]);
+
+	// 1 second passes.
+	b.update(1000.0f);
+
+	tz_assert(b.get_speed() == 9.81f, "Body speed changed but didn't have any acceleratsion. Expected %.2f, got %.2f", 9.81f, b.get_speed());
+	tz_assert(b.transform.position[1] = -9.81f, "Body did not move correctly when gravity was applied. Expected %.2f, got %.2f", -9.81f, b.transform.position[1]);
 }
