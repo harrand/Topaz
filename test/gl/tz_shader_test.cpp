4aa87f63f62553b8ba8ace8752fb1938f575884a
Harrand <harrysjh98@gmail.com>
2022-06-12 19:15:47 +0100

    * tz_shader_test is no longer empty. Instead, it runs a new compute shader designed to use asserts to test the TZSL standard library. It already found out that tz::matrix::decompose_quaternion was completely wrong. It has since been fixed and the test now passes

--- test/gl/tz_shader_test.cpp
+++ test/gl/tz_shader_test.cpp
@@ -1,1 +1,19 @@
-int main(){}
+#include "core/tz.hpp"
+#include "gl/device.hpp"
+#include "gl/renderer.hpp"
+#include "gl/imported_shaders.hpp"
+
+#include ImportedShaderHeader(shader_test, compute)
+
+int main()
+{
+	tz::initialise();
+	{
+		tz::gl::Device dev;
+		tz::gl::RendererInfo rinfo;
+		rinfo.shader().set_shader(tz::gl::ShaderStage::Compute, ImportedShaderSource(shader_test, compute));
+		tz::gl::Renderer renderer = dev.create_renderer(rinfo);
+		renderer.render(1);
+	}
+	tz::terminate();
+}
