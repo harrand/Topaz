60eda182d7bdf8b327b18f5189109370076166b4
Harrand <harrysjh98@gmail.com>
2022-10-21 02:40:18 +0100

    - Remove the ridiculous `wait test` in tz_time_test

--- test/core/time_test.cpp
+++ test/core/time_test.cpp
@@ -2,23 +2,6 @@
 #include "tz/core/assert.hpp"
 #include <thread>
 
-void test_waits()
-{
-	using namespace tz::literals;
-	using namespace std::chrono_literals;
-	tz::Delay d{10_ms};
-	tz_assert(!d.done(), "Delay of 10 millis was apparantly done instantly.");
-	std::this_thread::sleep_for(11ms);
-	tz_assert(d.done(), "Delay of 10 millis was not done after a this_thread_sleep_for 11ms");
-
-	d.reset();
-	tz_assert(!d.done(), "Delay of 10 millis was apparantly done instantly.");
-	tz_assert(!d, "Delay of 10 millis was apparantly done instantly.");
-	std::this_thread::sleep_for(11ms);
-	tz_assert(d.done(), "Delay of 10 millis was not done after a this_thread_sleep_for 11ms");
-	tz_assert(d, "Delay of 10 millis was not done after a this_thread_sleep_for 10ms");
-}
-
 int main()
 {
 	using namespace tz::literals;
@@ -30,5 +13,4 @@ int main()
 	static_assert(s.seconds<float>() == 1.0f);
 	static_assert(s.seconds<unsigned int>() == (1000_ms).seconds<float>());
 	static_assert(s > 500_ms && s < 2_s);
-	test_waits();
 }
