6c451319f21ea0af7820298d6440ec860e98e92f
Harrand <harrysjh98@gmail.com>
2022-11-05 17:05:38 +0100

    - Removed some dead code

--- src/tz/gl/impl/frontend/vk2/component.cpp
+++ src/tz/gl/impl/frontend/vk2/component.cpp
@@ -116,33 +116,6 @@ namespace tz::gl
 	{
 		(void)new_dimensions;
 		tz_error("ImageComponent::resize is deprecated. Use a renderer edit instead. If a renderer edit is using this function, it needs to do the resize itself.");
-		// Firstly, make a copy of the old image data.
-		std::vector<std::byte> old_data;
-		{
-			auto old_span = this->resource->data_as<std::byte>();
-			old_data.resize(old_span.size_bytes());
-			std::copy(old_span.begin(), old_span.end(), old_data.begin());
-		}
-		// Secondly, let's tell the ImageResource the new dimensions. It's data will be invalidated as soon as we do this.
-		auto* ires = static_cast<ImageResource*>(this->resource);
-		ires->set_dimensions(new_dimensions);
-		ires->resize_data(new_dimensions[0] * new_dimensions[1] * tz::gl::pixel_size_bytes(tz::gl::from_vk2(this->image.get_format())));
-		// Then, recreate the image. The image will have the new dimensions but undefined data contents.
-		std::string debug_name = this->image.debug_get_name();
-		this->image = make_image(this->image.get_device());
-		this->image.debug_set_name(debug_name);
-		/*
-		// After that, let's re-validate the resource data span. It will still have undefined contents for now.
-		auto new_data = this->vk_get_image().map_as<std::byte>();
-		this->resource->set_mapped_data(new_data);
-		// Finally, copy over the old data.
-		std::size_t copy_size = std::min(new_data.size_bytes(), old_data.size());
-		std::copy(old_data.begin(), old_data.begin() + copy_size, new_data.begin());
-		// If the image has grown, then the new texels will still have undefined values because the copy didnt cover the entire contents.
-		// Let's zero it all.
-		std::size_t num_new_texels = std::max(new_data.size_bytes(), old_data.size()) - copy_size;
-		std::fill_n(new_data.begin() + copy_size, num_new_texels, std::byte{0});
-		*/
 	}
 
 	const vk2::Image& ImageComponentVulkan::vk_get_image() const
