fcf46842759847da8637ec0c04f19c854b42fb26
Harrand <harrysjh98@gmail.com>
2022-11-14 22:13:37 +0000

    + Added impl source directory for vulkan. Add these sources and link against volk if we're configuring for a vulkan build

---
+++ src/impl/vulkan/tzvk.cpp
@@ -0,0 +1,15 @@
+#include "impl/vulkan/tzvk.hpp"
+#include "volk.h"
+
+namespace tz::impl::vk
+{
+	void initialise()
+	{
+
+	}
+
+	void terminate()
+	{
+
+	}
+}
---
+++ src/impl/vulkan/tzvk.hpp
@@ -0,0 +1,10 @@
+#ifndef VULKAN_TZVK_HPP
+#define VULKAN_TZVK_HPP
+
+namespace tz::impl::vk
+{
+	void initialise();
+	void terminate();
+}
+
+#endif // VULKAN_TZVK_HPP
