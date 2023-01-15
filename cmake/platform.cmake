8f26642bba66ae613723acf4b011c7ef81dc5f2c
Harrand <harrysjh98@gmail.com>
2023-01-15 23:49:13 +0000

    * CI Linux: Iteration 2 (bring in all recommended ogl libraries for all linux configs and set a cmake policy to prefer new GL)

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -6,6 +6,8 @@ function(configure_linux target)
 	# Linux via X11
 	message(WARNING "Support for Linux is not yet fully implemented")
 	find_package(X11 REQUIRED)
+	# Prefer new OGL over legacy.
+	cmake_policy(SET CMP0072 NEW)
 	find_package(OpenGL REQUIRED)
 	# Xrandr for retrieving monitor info.
 	target_link_libraries(${target} PUBLIC X11 X11::Xrandr OpenGL::GL)
