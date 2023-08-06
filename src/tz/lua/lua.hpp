2e00bea17d2e5aff05883d80f8fafcb0b6a12660
harrand <harrysjh98@gmail.com>
2023-08-06 21:10:06 +0100

    [lua] minor documentation pass for tz.version and the lua console

--- src/tz/lua/lua.hpp
+++ src/tz/lua/lua.hpp
@@ -3,11 +3,29 @@
 
 /**
 * @page tz_lua Lua API Reference
+* @section Introduction
+* - During the runtime of a debug-build, you can use the lua console within the dbgui to run lua ad-hoc (location: "Engine->Lua Console")
 * @section Functions
 * @code{.lua}
 * function tz.assert(expr: boolean)
 * @endcode
 * Asserts that an expression is true. If it is false, an assertion failure occurs on debug builds.
+* <hr>
+*
+* @section Constants
+* @code{.lua}
+* table tz.version
+* {
+* 	number major,
+* 	number minor,
+* 	number patch,
+* 	string string
+* }
+* @endcode
+* A table containing version info for the current version of the host engine.
+* @note `tz.version.string` represents a readable, printable Topaz version.
+*
+* <hr>
 */
 
 /**
