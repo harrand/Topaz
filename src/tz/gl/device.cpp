98aff7a4dc42a2b0ab7c6f9c115a2be88bf6a313
harrand <harrysjh98@gmail.com>
2023-09-30 20:24:12 +0100

    [lua] tz.gl.get_device() now uses a lua namespace under-the-hood as opposed to a manually-populated table

--- src/tz/gl/device.cpp
+++ src/tz/gl/device.cpp
@@ -35,42 +35,34 @@ namespace tz::gl
 
 	// LUA API
 
-	LUA_BEGIN(device_renderer_count)
-		state.stack_push_uint(tz::gl::get_device().renderer_count());
-		return 1;
-	LUA_END
-
-	LUA_BEGIN(device_get_renderer)
-		std::size_t rid = state.stack_get_uint(1);	
-		auto& ren = tz::gl::get_device().get_renderer(static_cast<tz::hanval>(rid));
-		state.stack_push_ref(ren);
-		return 1;
-	LUA_END
-
-	LUA_BEGIN(device_full_wait)
-		tz::gl::get_device().full_wait();
-		return 0;
-	LUA_END
-
-	LUA_BEGIN(device_frame_wait)
-		tz::gl::get_device().frame_wait();
-		return 0;
-	LUA_END
-
 	LUA_NAMESPACE_BEGIN(impl_tz_gl_device)
+
 		LUA_NAMESPACE_FUNC_BEGIN(renderer_count)
+			state.stack_push_uint(tz::gl::get_device().renderer_count());
 			return 1;
+		LUA_NAMESPACE_FUNC_END
+
+		LUA_NAMESPACE_FUNC_BEGIN(get_renderer)
+			std::size_t rid = state.stack_get_uint(1);	
+			auto& ren = tz::gl::get_device().get_renderer(static_cast<tz::hanval>(rid));
+			state.stack_push_ref(ren);
+			return 1;
+		LUA_NAMESPACE_FUNC_END
+
+		LUA_NAMESPACE_FUNC_BEGIN(full_wait)
+			tz::gl::get_device().full_wait();
+			return 0;
+		LUA_NAMESPACE_FUNC_END
+
+		LUA_NAMESPACE_FUNC_BEGIN(frame_wait)
+			tz::gl::get_device().frame_wait();
+			return 0;
 		LUA_NAMESPACE_FUNC_END
 	LUA_NAMESPACE_END
 
 	void lua_initialise_device(tz::lua::state& state)
 	{
-		state.assign_emptytable("tz.gl._impl_device");
-		state.assign_func("tz.gl._impl_device.renderer_count", LUA_FN_NAME(device_renderer_count));
-		state.assign_func("tz.gl._impl_device.get_renderer", LUA_FN_NAME(device_get_renderer));
-		state.assign_func("tz.gl._impl_device.full_wait", LUA_FN_NAME(device_full_wait));
-		state.assign_func("tz.gl._impl_device.frame_wait", LUA_FN_NAME(device_frame_wait));
-
-		state.execute("tz.gl.get_device = function() return tz.gl._impl_device end");
+		state.open_lib("tz_gl_impl_device", LUA_NAMESPACE_NAME(impl_tz_gl_device)::registers);
+		state.execute("tz.gl.get_device = function() return tz_gl_impl_device end");
 	}
 }
