1d97e7d7b0a5be0602cfcbb3eb14c070f8092417
harrand <harrysjh98@gmail.com>
2023-10-01 19:23:45 +0100

    [lua] minor refactoring of tz.gl.find_renderer_by_name

--- src/tz/gl/tz_gl.lua
+++ src/tz/gl/tz_gl.lua
@@ -1,6 +1,7 @@
 tz.gl.find_renderer_by_name = function(name)
-	if tz.gl.get_device().renderer_count() <= 0 then return nil end
-	for i=0,tz.gl.get_device().renderer_count()-1,1 do
+	local rc = tz.gl.get_device().renderer_count()
+	if rc <= 0 then return nil end
+	for i=0, rc-1, 1 do
 		local ren = tz.gl.get_device().get_renderer(i)
 		if ren:debug_get_name() == name then
 			return ren
