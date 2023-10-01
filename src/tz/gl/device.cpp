9cd81f428f339f20dbdad57a81635ce7299053ae
harrand <harrysjh98@gmail.com>
2023-10-01 19:55:18 +0100

    [lua.gl] tz.gl.get_device() now returns a proper object in lua, meaning colon syntax should now be used on it. i.e `ren = tz.gl.get_device():get_renderer(123)`

--- src/tz/gl/device.cpp
+++ src/tz/gl/device.cpp
@@ -35,26 +35,31 @@ namespace tz::gl
 	}
 
 	// LUA API
+	// new
 
 	struct impl_tz_gl_renderer
 	{
-		tz::gl::renderer& get()
+		impl_tz_gl_renderer(tz::gl::renderer_handle rh):
+		rh(rh){}
+
+		tz::gl::renderer& ren()
 		{
 			return tz::gl::get_device().get_renderer(this->rh);
 		}
-		
+
 		int resource_count(tz::lua::state& state)
 		{
-			state.stack_push_uint(this->get().resource_count());
+			state.stack_push_uint(this->ren().resource_count());
 			return 1;
-		}	
+		}
 
 		int debug_get_name(tz::lua::state& state)
 		{
-			state.stack_push_string(this->get().debug_get_name());
+			state.stack_push_string(this->ren().debug_get_name());
 			return 1;
 		}
-		tz::gl::renderer_handle rh = tz::nullhand;
+
+		tz::gl::renderer_handle rh;
 	};
 
 	LUA_CLASS_BEGIN(impl_tz_gl_renderer)
@@ -63,27 +68,33 @@ namespace tz::gl
 			LUA_METHOD(impl_tz_gl_renderer, debug_get_name)
 		LUA_CLASS_METHODS_END
 	LUA_CLASS_END
-
-	LUA_NAMESPACE_BEGIN(impl_tz_gl_device)
-		LUA_NAMESPACE_FUNC_BEGIN(renderer_count)
+	struct impl_tz_gl_device
+	{
+		int renderer_count(tz::lua::state& state)
+		{
 			state.stack_push_uint(tz::gl::get_device().renderer_count());
 			return 1;
-		LUA_NAMESPACE_FUNC_END
+		}
 
-		LUA_NAMESPACE_FUNC_BEGIN(get_renderer)
-			std::size_t rid = state.stack_get_uint(1);	
-			LUA_CLASS_PUSH(state, impl_tz_gl_renderer, {static_cast<tz::gl::renderer_handle>(static_cast<tz::hanval>(rid))});
+		int get_renderer(tz::lua::state& state)
+		{
+			auto [_, rid] = tz::lua::parse_args<tz::lua::nil, unsigned int>(state);
+			LUA_CLASS_PUSH(state, impl_tz_gl_renderer, impl_tz_gl_renderer{static_cast<tz::gl::renderer_handle>(static_cast<tz::hanval>(rid))});
 			return 1;
-		LUA_NAMESPACE_FUNC_END
+		}
+	};
 
-		LUA_NAMESPACE_FUNC_BEGIN(full_wait)
-			tz::gl::get_device().full_wait();
-			return 0;
-		LUA_NAMESPACE_FUNC_END
+	LUA_CLASS_BEGIN(impl_tz_gl_device)
+		LUA_CLASS_METHODS_BEGIN
+			LUA_METHOD(impl_tz_gl_device, renderer_count)
+			LUA_METHOD(impl_tz_gl_device, get_renderer)
+		LUA_CLASS_METHODS_END
+	LUA_CLASS_END
 
-		LUA_NAMESPACE_FUNC_BEGIN(frame_wait)
-			tz::gl::get_device().frame_wait();
-			return 0;
+	LUA_NAMESPACE_BEGIN(impl_tz_gl)
+		LUA_NAMESPACE_FUNC_BEGIN(get_device)
+			LUA_CLASS_PUSH(state, impl_tz_gl_device, {});
+			return 1;
 		LUA_NAMESPACE_FUNC_END
 	LUA_NAMESPACE_END
 
@@ -91,9 +102,10 @@ namespace tz::gl
 	{
 		// LUA_CLASS_REGISTER_ONE basically
 		state.new_type("impl_tz_gl_renderer", LUA_CLASS_NAME(impl_tz_gl_renderer)::registers);
+		state.new_type("impl_tz_gl_device", LUA_CLASS_NAME(impl_tz_gl_device)::registers);
 
 		// LUA_NAMESPACE_REGISTER_ONE basically
-		state.open_lib("tz_gl_impl_device", LUA_NAMESPACE_NAME(impl_tz_gl_device)::registers);
-		state.execute("tz.gl.get_device = function() return tz_gl_impl_device end");
+		state.open_lib("impl_tz_gl", LUA_NAMESPACE_NAME(impl_tz_gl)::registers);
+		state.execute("tz.gl = impl_tz_gl");
 	}
 }
--- src/tz/gl/tz_gl.lua
+++ src/tz/gl/tz_gl.lua
@@ -1,8 +1,8 @@
 tz.gl.find_renderer_by_name = function(name)
-	local rc = tz.gl.get_device().renderer_count()
+	local rc = tz.gl.get_device():renderer_count()
 	if rc <= 0 then return nil end
 	for i=0, rc-1, 1 do
-		local ren = tz.gl.get_device().get_renderer(i)
+		local ren = tz.gl.get_device():get_renderer(i)
 		if ren:debug_get_name() == name then
 			return ren
 		end
