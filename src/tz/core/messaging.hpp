245035ce9e6671cb65fe3bd6ad66b4dba04ea4a4
harrand <harrysjh98@gmail.com>
2024-01-22 15:59:13 +0000

    [tz.core.messaging] message_receiver is now conditionally-thread-safe, i.e you can opt-out of the mutex and locks

--- src/tz/core/messaging.hpp
+++ src/tz/core/messaging.hpp
@@ -7,27 +7,54 @@
 
 namespace tz
 {
-	template<tz::message M>
-	class message_receiver
+	namespace detail
+	{
+		// base class for a non-thread-safe receiver.
+		// lock returns empty lock type (i.e doesnt lock anything)
+		struct no_lock
+		{
+		protected:
+			using lock_t = std::unique_lock<std::mutex>;
+			lock_t lock() const{return {};}
+		};
+
+		// base class for a thread-safe receiver.
+		// lock returns a unique lock to the internal mutex.
+		struct with_lock
+		{
+		protected:
+			std::mutex mtx = {};	
+			using lock_t = std::unique_lock<std::mutex>;
+			lock_t lock() {return lock_t{this->mtx};}
+		};
+	}
+
+	/**
+	 * Conditionally thread-safe message receiver.
+	 * @tparam M Message Type. Must satisfy `tz::message<M>`
+	 * @tparam thread_safe Whether the object should lock an internal mutex. Defaults to true, but you might want to set this to false for a thread-local receiver, for example.
+	 */
+	template<tz::message M, bool thread_safe = true>
+	class message_receiver : protected std::conditional_t<thread_safe, detail::with_lock, detail::no_lock>
 	{
 	public:
-		/// Send a message to the receiver. Thread-safe.
+		/// Send a message to the receiver. Conditionally thread-safe.
 		void send_message(M msg);
-		/// Send multiple messages at once. Thread-safe.
+		/// Send multiple messages at once. Conditionally thread-safe.
 		void send_messages(tz::range_of<M> auto range);
-		/// Retrieve the number of messages in the receiver's queue. Thread-safe.
+		/// Retrieve the number of messages in the receiver's queue. Conditionally thread-safe.
 		std::size_t message_count() const;
 	protected:
 		// Processes all messages and then clears the list.
 		void process_messages();
-		/// Invoked when a message is sent. Your override can return false to drop the message. By default, no messages are dropped.
+		/// Invoked when a message is sent. Your override can return false to drop the message. By default, no messages are dropped. Not thread safe.
 		virtual bool on_send_message(const M& msg) {return true;};
-		/// Invoked when a message is processed. You must override this.
+		/// Invoked when a message is processed. You must override this. Not thread safe.
 		/// @note If processing a message causes the receiver to send a new message to itself, this is safe, but will not be processed until the next invocation to `process_messages`.
 		constexpr virtual void process_message(const M& msg) = 0;
-
+	private:
+		using base_t = std::conditional_t<thread_safe, detail::with_lock, detail::no_lock>;
 		std::vector<M> messages = {};
-		std::mutex mtx = {};
 	};
 }
 
--- src/tz/core/messaging.inl
+++ src/tz/core/messaging.inl
@@ -1,18 +1,20 @@
 namespace tz
 {
-	template<tz::message M>
-	void message_receiver<M>::send_message(M msg)
+	template<tz::message M, bool thread_safe>
+	void message_receiver<M, thread_safe>::send_message(M msg)
 	{
 		if(!this->on_send_message(msg))
 		{
 			return;
 		}
-		std::unique_lock<std::mutex> lock(this->mtx);
+
+		typename base_t::lock_t lock = base_t::lock();
+	
 		this->messages.push_back(msg);
 	}
 
-	template<tz::message M>
-	void message_receiver<M>::send_messages(tz::range_of<M> auto range)
+	template<tz::message M, bool thread_safe>
+	void message_receiver<M, thread_safe>::send_messages(tz::range_of<M> auto range)
 	{
 		std::vector<M> non_dropped_messages;
 		non_dropped_messages.reserve(std::ranges::size(range));
@@ -25,27 +27,27 @@ namespace tz
 		}
 		if(non_dropped_messages.size())
 		{
-			std::unique_lock<std::mutex> lock(this->mtx);
+			typename base_t::lock_t lock = base_t::lock();
 			this->messages.insert(this->messages.end(), non_dropped_messages.begin(), non_dropped_messages.end());
 		}
 	}
 	
 
-	template<tz::message M>
-	std::size_t message_receiver<M>::message_count() const
+	template<tz::message M, bool thread_safe>
+	std::size_t message_receiver<M, thread_safe>::message_count() const
 	{
-		std::unique_lock<std::mutex> lock(this->mtx);
+		typename base_t::lock_t lock = base_t::lock();
 		return this->messages.size();
 	}
 
-	template<tz::message M>
-	void message_receiver<M>::process_messages()
+	template<tz::message M, bool thread_safe>
+	void message_receiver<M, thread_safe>::process_messages()
 	{
 		using message_container_t = decltype(this->messages);
 		message_container_t msgs = {};
 		// lock for as short a time as possible. move messages into our own buffer and then relinquish lock.
 		{
-			std::unique_lock<std::mutex> lock(this->mtx);
+			typename base_t::lock_t lock = base_t::lock();
 			msgs = std::move(this->messages);
 			tz::assert(this->messages.empty());
 			//this->messages.clear();
