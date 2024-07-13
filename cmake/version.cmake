1283877e30b31d0a40931aacdcf3fdf00cd01f3e
Harrand <harrysjh98@gmail.com>
2024-07-13 16:04:35 +0100

    [cmake] handle versions

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,3 +1,16 @@
-configure_file(${CMAKE_SOURCE_DIR}/version.txt version_txt_dummy.xml)
-file(READ ${CMAKE_SOURCE_DIR}version.txt TOPAZ_VERSION_TXT)
-string(REGEX MATCH "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" TOPAZ_VERSION ${TOPAZ_VERSION_TXT})
+set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${CMAKE_SOURCE_DIR}/version.txt)
+file(READ ${CMAKE_SOURCE_DIR}/version.txt TOPAZ_VERSION_TXT)
+
+# Search for a version (MAJOR.MINOR) within a string.
+# e.g parse_version(my_major my_minor "application version: 123.45")
+# my_major = 123
+# my_minor = 45
+function(parse_version out_major out_minor large_string)
+	string(REGEX MATCH "[0-9]+\.[0-9]+" version_string ${large_string})
+	string(REPLACE "." ";" version_list ${version_string})
+	list(GET version_list 0 _major)
+	list(GET version_list 1 _minor)
+	set(${out_major} ${_major} PARENT_SCOPE)
+	set(${out_minor} ${_minor} PARENT_SCOPE)
+endfunction()
+parse_version(TOPAZ_MAJOR_VERSION TOPAZ_MINOR_VERSION ${TOPAZ_VERSION_TXT})
