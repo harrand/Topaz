8a3aa185d849f4328c679842a1a461dbeb36a340
Harrand <harrysjh98@gmail.com>
2024-09-21 19:26:30 +0100

    [docs] added docs subdirectory aswell as doxyfile

--- include/tz/topaz.hpp
+++ include/tz/topaz.hpp
@@ -1,12 +1,22 @@
 #ifndef TOPAZ_HPP
 #define TOPAZ_HPP
 
+/**
+ * @defgroup topaz Topaz Engine
+ * @{
+**/
 namespace tz
 {
+	/// Initialise Topaz.
 	void initialise();
+	/// Terminate Topaz.
 	void terminate();
 }
 
 #include "detail/debug.hpp"
+
+/**
+ * @}
+**/
 
 #endif // TOPAZ_HPP
