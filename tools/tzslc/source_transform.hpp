f64b52dc59a3a9d3b1a613c9010aba9e1d4957e7
Harrand <harrysjh98@gmail.com>
2024-10-11 19:44:38 +0100

    [tools] re-added tzslc from Topaz 4.2

---
+++ tools/tzslc/source_transform.hpp
@@ -0,0 +1,66 @@
+#ifndef TZSLC_SOURCE_TRANSFORM_HPP
+#define TZSLC_SOURCE_TRANSFORM_HPP
+#include "assert.hpp"
+#include <string>
+#include <vector>
+#include <regex>
+
+namespace tzslc
+{
+	namespace detail
+	{
+		using SourceMatchIteratorType = std::vector<std::string>::iterator;
+		template<typename F, typename Result, typename... Args>
+		concept Function = requires(F f, Args... args)
+		{
+			{f(args...)} -> std::convertible_to<Result>;
+		};
+	}
+	template<typename F>
+	concept SourceReplaceAction = detail::Function<F, std::string, detail::SourceMatchIteratorType, detail::SourceMatchIteratorType>;
+	bool transform(std::string& source, std::regex reg, SourceReplaceAction auto transform_function)
+	{
+		using ReplaceJob = std::pair<std::pair<std::size_t, std::size_t>, std::string>;
+		std::vector<ReplaceJob> replacements;
+		
+		std::string src_copy = source;
+		std::smatch sm;
+		std::size_t src_pos_counter = 0;
+
+		bool did_any_work = false;
+
+		while(std::regex_search(src_copy, sm, reg))
+		{
+			// Found a match!
+			std::size_t pos = sm.position() + src_pos_counter;
+			tzslc_assert(pos < source.size(), "Match result has position %zu in source which is out of range. Size: %zu", pos, source.size());
+			std::size_t len = sm.length();
+			std::vector<std::string> inner_matches;
+			for(std::size_t i = 1; i < sm.size(); i++)
+				inner_matches.push_back(sm[i]);
+			src_copy = sm.suffix();
+			src_pos_counter = source.find(src_copy);
+			//src_pos_counter += pos + len;
+			// get the transformed string.
+			std::string replacement = transform_function(inner_matches.begin(), inner_matches.end());
+			// register this source replacement.
+			ReplaceJob job{{pos, len}, replacement};
+			replacements.push_back(job);
+		}
+
+		did_any_work = !replacements.empty();
+
+		// Invoke all replacements (in reverse-order)
+		for(auto i = replacements.rbegin(); i != replacements.rend(); i++)
+		{
+			std::string replacement = i->second;
+			std::size_t pos = i->first.first;
+			std::size_t len = i->first.second;
+			source.replace(pos, len, replacement);
+		}
+
+		return did_any_work;
+	}
+}
+
+#endif // TZSLC_SOURCE_TRANSFORM_HPP
---
+++ tools/tzslc/stdlib.hpp
@@ -0,0 +1,28 @@
+#ifndef TOPAZ_TZSLC_STDLIB_HPP
+#define TOPAZ_TZSLC_STDLIB_HPP
+#include "textc/imported_text.hpp"
+#include <string_view>
+#include <span>
+
+#include ImportedTextHeader(atomic, tzsl)
+#include ImportedTextHeader(debug, tzsl)
+#include ImportedTextHeader(draw, tzsl)
+#include ImportedTextHeader(space, tzsl)
+#include ImportedTextHeader(math, tzsl)
+#include ImportedTextHeader(matrix, tzsl)
+#include ImportedTextHeader(mesh, tzsl)
+#include ImportedTextHeader(noise, tzsl)
+
+namespace tzslc
+{
+	const std::string_view stdlib_atomic = ImportedTextData(atomic, tzsl);
+	const std::string_view stdlib_debug = ImportedTextData(debug, tzsl);
+	const std::string_view stdlib_draw = ImportedTextData(draw, tzsl);
+	const std::string_view stdlib_space = ImportedTextData(space, tzsl);
+	const std::string_view stdlib_noise = ImportedTextData(noise, tzsl);
+	const std::string_view stdlib_math = ImportedTextData(math, tzsl);
+	const std::string_view stdlib_matrix = ImportedTextData(matrix, tzsl);
+	const std::string_view stdlib_mesh = ImportedTextData(mesh, tzsl);
+}
+
+#endif // TOPAZ_TZSLC_STDLIB_HPP
