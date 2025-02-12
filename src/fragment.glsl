623ae5047289a8c9a2f423261ba7d2a171c58a01
harrand <harrandev@gmail.com>
2025-02-12 23:34:10 +0000

    [file] added file api, aswell as moved shaders into external files embedded at compile-time via __embed

---
+++ src/fragment.glsl
@@ -0,0 +1,6 @@
+#version 430
+layout(location = 0) out vec4 fcol;
+void main()
+{
+	fcol = vec4(1.0, 0.0, 0.0, 1.0);
+}
---
+++ src/vertex.glsl
@@ -0,0 +1,6 @@
+#version 430
+vec2 positions[3] = vec2[](vec2(0.0, 0.5), vec2(0.5, -0.5), vec2(-0.5, -0.5));
+void main()
+{
+	gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+}
