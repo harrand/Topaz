f5366aa791f654753949f068a178492b37f32458
harrand <harrandev@gmail.com>
2025-03-02 21:23:48 +0000

    [gpu] implemented tz_gpu_resource_write to write to a resource

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,19 +1,8 @@
 #version 430
-#extension GL_EXT_buffer_reference : require
 #extension GL_EXT_nonuniform_qualifier : require
 #pragma shader_stage fragment
 layout(location = 0) in vec2 uv;
 layout(location = 0) out vec4 fcol;
-
-layout(std430, buffer_reference, buffer_reference_align = 8) readonly buffer color_t
-{
-	vec4 value;
-};
-
-layout(std430, set = 0, binding = 0) readonly buffer MetaBuffer
-{
-	color_t color;
-};
 
 layout(binding = 1) uniform sampler2D tz_textures[];
 #define sample(id, uv) texture(tz_textures[id], uv)
@@ -21,5 +10,5 @@ layout(binding = 1) uniform sampler2D tz_textures[];
 
 void main()
 {
-	fcol = color.value * sample(0, uv);
+	fcol = sample(0, uv);
 }
