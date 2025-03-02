8ac5462858a517edd103c8674342e3cc60a1f6c8
harrand <harrandev@gmail.com>
2025-03-02 17:00:35 +0000

    [gpu.vk] metabuf seems to work correctly when buffer resources are now present. i am able to write a triangle colour into a buffer resouce and use it in the shader.

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,7 +1,20 @@
 #version 430
+#extension GL_EXT_buffer_reference : require
 #pragma shader_stage fragment
 layout(location = 0) out vec4 fcol;
+
+layout(std430, buffer_reference, buffer_reference_align = 8) readonly buffer color_t
+{
+	vec4 value;
+};
+
+layout(std430, set = 0, binding = 0) readonly buffer MetaBuffer
+{
+	color_t color;
+};
+
 void main()
 {
-	fcol = vec4(1.0, 0.0, 0.0, 1.0);
+	fcol = color.value;
+	//fcol = vec4(1.0, 0.0, 1.0, 1.0);
 }
