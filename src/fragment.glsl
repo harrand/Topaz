73d6f9fd41741e4911adc80b4bece3895ce25fdc
harrand <harrandev@gmail.com>
2025-03-02 19:16:46 +0000

    [gpu] buffers and images now seem to work

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,6 +1,8 @@
 #version 430
 #extension GL_EXT_buffer_reference : require
+#extension GL_EXT_nonuniform_qualifier : require
 #pragma shader_stage fragment
+layout(location = 0) in vec2 uv;
 layout(location = 0) out vec4 fcol;
 
 layout(std430, buffer_reference, buffer_reference_align = 8) readonly buffer color_t
@@ -13,8 +15,11 @@ layout(std430, set = 0, binding = 0) readonly buffer MetaBuffer
 	color_t color;
 };
 
+layout(binding = 1) uniform sampler2D tz_textures[];
+#define sample(id, uv) texture(tz_textures[id], uv)
+#define texsize(id) textureSize(tz_textures[id], 0)
+
 void main()
 {
-	fcol = color.value;
-	//fcol = vec4(1.0, 0.0, 1.0, 1.0);
+	fcol = color.value * sample(0, uv);
 }
