7159c938e554fcaccab21d76705003355e82dd0d
harrand <harrandev@gmail.com>
2025-03-07 00:49:06 +0000

    [mesh] store vertex and index data properly. no draw indirect buffer yet though. tz_gpu now enables scalar block layout

--- src/mesh_renderer_vertex.glsl
+++ src/mesh_renderer_vertex.glsl
@@ -1,23 +1,30 @@
 #version 430
 #extension GL_EXT_buffer_reference : require
+#extension GL_EXT_scalar_block_layout : require
 #pragma shader_stage vertex
 vec2 positions[3] = vec2[](vec2(0.0, 0.5), vec2(0.5, -0.5), vec2(-0.5, -0.5));
 vec2 texcoords[3] = vec2[](vec2(0.5, 1.0), vec2(1.0, 0.0), vec2(0.0, 0.0));
 
-layout(std430, buffer_reference, buffer_reference_align = 8) readonly buffer pos_t
+struct vertex_t
 {
-	vec4 value;
+	vec3 pos;
+	vec2 texcoord;
+};
+
+layout(scalar, buffer_reference, buffer_reference_align = 8) readonly buffer vertex_data_t
+{
+	vertex_t data[];
 };
 
 layout(std430, set = 0, binding = 0) readonly buffer MetaBuffer
 {
-	pos_t pos;
+	vertex_data_t vertex;
 };
-
 
 layout(location = 0) out vec2 uv;
 void main()
 {
-	gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0) + pos.value;
-	uv = texcoords[gl_VertexIndex];
+	vertex_t cur_vertex = vertex.data[gl_VertexIndex];
+	gl_Position = vec4(cur_vertex.pos, 1.0);
+	uv = cur_vertex.texcoord;
 }
