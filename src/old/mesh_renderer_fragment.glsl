4860d14bdfe28779cdb0dcd00e3e0cc48d106ae7
harrand <harrandev@gmail.com>
2025-06-19 23:19:34 +0100

    [src] removed old directory as we are built using the newest bootstrap compiler

--- src/old/mesh_renderer_fragment.glsl
+++
@@ -1,8 +0,0 @@
-#version 460 core
-#extension GL_EXT_nonuniform_qualifier : require
-#pragma shader_stage fragment
-layout(location = 0) out vec4 fcol;
-void main()
-{
-	fcol = vec4(1.0, 0.0, 0.0, 1.0);
-}
