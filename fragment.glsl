2e60bd0cba6b9002b153471dd3adc0b36e6a46d8
harrand <harrandev@gmail.com>
2025-06-19 20:50:26 +0100

    [gpu] added tz_gpu api which now works correctly

--- fragment.glsl
+++ fragment.glsl
@@ -1,14 +1,9 @@
 #version 430
 #extension GL_EXT_nonuniform_qualifier : require
 #pragma shader_stage fragment
-layout(location = 0) in vec2 uv;
 layout(location = 0) out vec4 fcol;
-
-layout(binding = 1) uniform sampler2D tz_textures[];
-#define sample(id, uv) texture(tz_textures[id], uv)
-#define texsize(id) textureSize(tz_textures[id], 0)
 
 void main()
 {
-	fcol = sample(0, uv);
+	fcol = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -4,20 +4,7 @@
 vec2 positions[3] = vec2[](vec2(0.0, 0.5), vec2(0.5, -0.5), vec2(-0.5, -0.5));
 vec2 texcoords[3] = vec2[](vec2(0.5, 1.0), vec2(1.0, 0.0), vec2(0.0, 0.0));
 
-layout(std430, buffer_reference, buffer_reference_align = 8) readonly buffer pos_t
-{
-	vec4 value;
-};
-
-layout(std430, set = 0, binding = 0) readonly buffer MetaBuffer
-{
-	pos_t pos;
-};
-
-
-layout(location = 0) out vec2 uv;
 void main()
 {
-	gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0) + pos.value;
-	uv = texcoords[gl_VertexIndex];
+	gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
 }
